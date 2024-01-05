ca9dd0f11b424e671c963915b1defddd4919c306
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-01-05 11:16:02 -0600

    task 2, implements class with cosntructor attributes

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError ('Length must be a number');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('must be a array');
+    } else {
+      this._students = students;
+    }
+  }
+  get name() {
+    return this._name;
+  }
+  set name(na) {
+    if (typeof na !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = na;
+  }
+  get length() {
+    return this._length;
+  }
+  set length(zsize) {
+    if (typeof zsize !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = zsize;
+  }
+  get students() {
+    return this._students;
+  }
+  set students(studs) {
+    if (!Array.isArray(studs)) {
+      throw new TypeError('must be a array');
+    }
+    this._students = studs;
+  }
+}
