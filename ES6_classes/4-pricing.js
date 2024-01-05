9503248814fa530a808e82f819fc524d7c4239fb
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-01-05 11:44:07 -0600

    fixed double letter typos

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,11 +1,11 @@
 export default class Pricing {
   constructor(amount, currency) {
-    this._ammount = amount;
+    this._amount = amount;
     this._currency = currency;
   }
 
   get amount() {
-    return this._ammount;
+    return this._amount;
   }
 
   get currency() {
