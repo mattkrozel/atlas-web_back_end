93161ad7c248463e9b82e6eebb31f7a0dcde41fc
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-01-05 11:21:31 -0600

    fixed linter

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -6,7 +6,7 @@ export default class HolbertonCourse {
       this._name = name;
     }
     if (typeof length !== 'number') {
-      throw new TypeError ('Length must be a number');
+      throw new TypeError('Length must be a number');
     } else {
       this._length = length;
     }
@@ -16,27 +16,33 @@ export default class HolbertonCourse {
       this._students = students;
     }
   }
+
   get name() {
     return this._name;
   }
+
   set name(na) {
     if (typeof na !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = na;
   }
+
   get length() {
     return this._length;
   }
+
   set length(zsize) {
     if (typeof zsize !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = zsize;
   }
+
   get students() {
     return this._students;
   }
+  
   set students(studs) {
     if (!Array.isArray(studs)) {
       throw new TypeError('must be a array');
