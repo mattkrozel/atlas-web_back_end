9906e79543a71b83f9b7a87df6edd332ffc8e9e0
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-01-09 14:23:28 -0600

    pushed task 2, returning arrary of objects from specific city

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+const getStudentsByLocation = (students, city) => {
+  const studentsByLocation = students.filter(
+    (student) => student.location === city,
+  );
+  return studentsByLocation;
+};
+export default getStudentsByLocation;
