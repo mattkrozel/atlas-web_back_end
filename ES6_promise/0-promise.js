58f34e6b51a3903d416bd8d83a958ef92698b4d8
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-01-10 15:40:29 -0600

    fixed task 0 execution

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,4 +1,11 @@
 /* eslint-disable */
 export default function getResponseFromAPI() {
-  return new Promise((resolve) => {resolve('');});
+  return new Promise((resolve, reject) => {
+    const decider = true;
+    if (decider) {
+      resolve('true');
+    } else {
+      reject(new Error('false error message'));
+    }
+  });
 }
