2b5c8b178a5e6ba7817bff34f7d701651dc45c0a
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-01-10 15:06:17 -0600

    task 10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  } else {
+    for (const [key, val] of map) {
+      if (val === 1) {
+        map.set(key, 100);
+      }
+    }
+    return map;
+  }
+}
