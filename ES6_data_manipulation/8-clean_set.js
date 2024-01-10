d9690ea14a854f1f70818a1738118b9d591bd911
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-01-10 14:04:54 -0600

    task 8 returns values that start with specific string

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanset(set, startString) {
+  const str = [];
+  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      str.push(item.slice(startString.length));
+    }
+  }
+  return str.join('-');
+}
