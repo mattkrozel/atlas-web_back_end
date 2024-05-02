004d226ae5a62ee1fb2361e13fa6933944a25935
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-02 14:30:38 -0500

    to str task push complete

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+'''
+type annotated function to str
+'''
+
+
+def to_str(n:float) -> str:
+    '''
+    args: n (float): float num
+    returns: str: string rep of the float
+    '''
+    return str(n)
