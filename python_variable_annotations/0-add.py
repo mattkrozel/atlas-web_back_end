e91f8438f4e973f37a6a0e94f41d0402c5bc5e2f
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-02 13:57:22 -0500

    push first task

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+'''
+function add
+@a: float
+b: float
+returns: sum of floats
+'''
+
+def add(a: float, b: float) -> float:
+    '''
+    add function
+    '''
+    return a + b
