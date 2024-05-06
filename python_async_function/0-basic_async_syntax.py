d783d80f416f9640e524500bbf292b5388a0e480
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-05 23:30:49 -0500

    all folder push

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+basics of async
+'''
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''
+    ansyncrhonoys courotine
+    args max_delay
+    returns float
+    '''
+
+    actual_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(actual_delay)
+    return actual_delay
