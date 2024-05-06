318b4ded3990e3182f11ad8c6b4481dc156124ef
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-06 05:54:39 -0500

    push new repo and task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+coroutine called async gen no args
+'''
+import asyncio
+import random
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    '''
+    coroutine will loop 10 times
+    each time delay 1 sec
+    yield random number betwen 0-10
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
