de6584c69d987f75aa1b448fad12981194b14595
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-05 23:02:49 -0500

    task 5 push

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+type annotated function sum_list
+'''
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    type annotated function sumlist
+    
+    args: input_list (List[Float]): lost of floats
+    
+    returns: float: sum as a float
+    '''
+    return sum(input_list)
