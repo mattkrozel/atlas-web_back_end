2d59179d63170ec2befab8273ea4b520393b1277
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-05 23:16:40 -0500

    task 7 push

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''
+type annotated function to_kv
+'''
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''
+    type annotated function to_kv
+    
+    args: 
+        k str
+        v union
+    
+    returns: tuple
+    '''
+    return (k, v ** 2)
