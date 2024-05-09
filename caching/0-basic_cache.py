3b92c299b5558c50db5e9722e07d9ffe7983fb48
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-09 13:44:43 -0500

    push new repo and starter files

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''
+Create a class BasicCache that inherits
+from BaseCaching and is a caching system
+'''
+
+from typing import Union
+BaseCaching = __import__('base_caching').BaseCaching
