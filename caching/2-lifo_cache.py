783224a92c714962a7a3f9918545fed0a21906d2
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-23 11:51:18 -0500

    inherits lifo class from base caching task 2

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python3
+'''
+Create a class LifoCache that inherits
+from BaseCaching
+'''
+
+BaseCaching = __import__('base_cachong').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    '''
+    lifo class implemtn
+    args basecaching class
+    '''
+    def __init__(self):
+        self.stack = []
+        super().__init__()
+
+    def put(self, key, item):
+        '''
+        assigns item to dictionary
+        '''
+        if key and item:
+            if self.cache_data.get(key):
+                self.stack.remove(key)
+            while len(self.stack) >= self.MAX_ITEMS:
+                delete = self.stack.pop()
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+            self.stack.append(key)
+            self.cache_data[key] = item
+  
+
+    def get(self, key: str) -> Union[None, object]:
+        '''
+        gets value of cache
+        '''
+        return self.cache_data.get[key]
