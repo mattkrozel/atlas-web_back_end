86ffded91e7f0e92a6b66073240070d854a8c4e6
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-23 11:06:50 -0500

    push task 0 class that inherits from basecaching

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -6,3 +6,24 @@ from BaseCaching and is a caching system
 
 from typing import Union
 BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    '''
+    cache class implemtn
+    args basecaching class
+    '''
+    def put(self, key: str, item: str) -> None:
+        '''
+        adds value in cache
+        '''
+        if key and item:
+            self.cache_data[key] = item
+
+
+    def get(self, key: str) -> Union[None, object]:
+        '''
+        gets value of cache
+        '''
+        if key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
