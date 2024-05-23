8fde870d32d9c9ce0253b2f69c1f9e76f9760d13
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-23 11:31:45 -0500

    create fifo class inherits form base caching task 1

--- caching/1-fifo_cache.py
+++ caching/1-fifo_cache.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+'''
+Create a class FifoCache that inherits
+from BaseCaching
+'''
+
+from typing import Union
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''
+    fifo class implemtn
+    args basecaching class
+    '''
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key: str, item: str) -> None:
+        '''
+        adds value in cache
+        '''
+        if key and item:
+            new_dict = {key: item}
+            self.isFillCache()
+            new_dict.update(self.cache_data)
+            self.cache_data = new_dict
+
+    def get(self, key: str) -> Union[None, object]:
+        '''
+        gets value of cache
+        '''
+        if key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
+    
+    def isFillCache(self) -> None:
+        '''
+        checks if cache is not full
+        '''
+        if len(self.cache_data.keys()) >= self.MAX_ITEMS:
+            delete_item = self.cache_data.popitem()[0]
+            print(f'DISCARD: {delete_item}')
+
