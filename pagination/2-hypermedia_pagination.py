07c6edb9b0090f8cd0ce3a647d6b85cb373ee546
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-26 18:14:23 -0500

    push task 2, added get hyper to return pagination elements

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 '''
-simple pagination
+hypermedia pagination
 '''
 import csv
 import math
-from typing import List
+from typing import Dict, List
 
 index_range = __import__('0-simple_helper_function').index_range
 
@@ -40,3 +40,20 @@ class Server:
         assert(page > 0 and page_size > 0)
         beginning, end = index_range(page, page_size)
         return self.dataset()[beginning:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        '''
+        get_hyper
+        args pag int, page_size int
+        returns dict of pagintatin elements
+        '''
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        new_dictionary = {
+            'page_size': page_size,
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
+        return new_dictionary
