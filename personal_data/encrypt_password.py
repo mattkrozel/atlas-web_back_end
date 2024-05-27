637dce3b30f95f0c49510cf75a44624507d72e7d
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-26 19:55:53 -0500

    personal_data task 6 validate matching passwords

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     encrypts password
     '''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''
+    encrypts password
+    '''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
