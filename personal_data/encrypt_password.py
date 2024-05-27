09012b94dd3820dd1b833851de89d2d8cbba685d
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-26 19:53:11 -0500

    personal_data task 5 hash pass, encrypt password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+'''
+encrypting password
+'''
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    '''
+    encrypts password
+    '''
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
