279573e72047aa8bbd3e46b0ccf5fa1325a269c8
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-26 22:59:11 -0500

    created class sessionauth and inherited

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -22,6 +23,8 @@ if getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
 elif getenv('AUTH_TYPE') == 'basic_auth':
     auth = BasicAuth()
+elif getenv('AUTH_TYPE') == 'session_auth':
+    auth = SessionAuth()
 
 
 @app.before_request
