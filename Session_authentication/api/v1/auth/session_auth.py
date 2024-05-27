574ff84af1c616d2eb2802643609fed7def761a5
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-26 23:37:24 -0500

    created method that returns user based on cookies

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,10 @@
 '''
 session auth module
 '''
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -34,3 +36,14 @@ class SessionAuth(Auth):
         if not session_id or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        '''
+        current_user
+        args request type
+        '''
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
