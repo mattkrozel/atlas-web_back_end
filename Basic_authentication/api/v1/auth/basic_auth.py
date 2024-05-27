9707b5e27751f570a034c7c2ba0bcff9341eb403
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-26 21:54:35 -0500

    task 8

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ module of bbasic auth
 '''
 from api.v1.auth.auth import Auth
 import re
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,32 @@ class BasicAuth(Auth):
         header = authorization_header.split(' ')
         return (None if not bool(re.search('^Basic ', authorization_header))
                 else header[1])
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        '''
+        arguments str
+        returns decoded value base64 str
+        '''
+        if (not base64_authorization_header or
+            type(base64_authorization_header) != str):
+            return None
+        try:
+            msg = base64.b64decode(base64_authorization_header.encode('utf-8'))
+            return msg.decode('utf-8')
+        except Exception:
+            return None
+        
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        '''
+        args self type
+        returns str type
+        '''
+        if (not decoded_base64_authorization_header
+            or type(decoded_base64_authorization_header) != str):
+            return (None, None)
+        credentials = decoded_base64_authorization_header.split(':', 1)
+        return (credentials[0], credentials[1]) if ':' in\
+            decoded_base64_authorization_header else (None, None)
