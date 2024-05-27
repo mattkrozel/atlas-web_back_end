ce9cbd79e63610db147de16d1029adeafa7337ae
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-26 19:09:24 -0500

    task 1 push filtered logger, added code and impemented format method

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -10,6 +10,27 @@ from os import getenv
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''
+        format method
+        args record
+        returns str
+        '''
+        msg = filter_datum(self.fields, self.REDACTION,
+                           super().format(record), self.SEPARATOR)
+        return msg
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
