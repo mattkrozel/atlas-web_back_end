2d9bffbfa6b9210c6d07c8199c22f4a2c607371b
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-26 21:16:10 -0500

    task 3, make class to manage api auth

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''
+module of auth
+'''
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    '''
+    manages api authetnication
+    '''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''
+        public method requiring auth
+        arguments oath str and excluded_paths list str
+        returns boolean
+        '''
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        '''
+        public method
+        args requst type
+        returns str request object
+        '''
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''
+        current users method
+        '''
+        return None
