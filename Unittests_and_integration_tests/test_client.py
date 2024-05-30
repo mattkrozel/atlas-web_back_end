0e66525acb2a36836001d80fff329162f8942ce9
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-30 17:34:50 -0500

    implemented unittest to check license

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -56,3 +56,14 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, check)
             mock_public.assert_called_once()
             mock_json.assert_called_once()
+
+    @parameterized.expand(
+        [({'license': {'key': 'my_license'}}, 'my_license', True),
+         ({'license': {'key': 'other_license'}}, 'my_license', False)]
+    )
+    def test_has_license(self, repo, license_key, expected):
+        '''
+        unittesting githuborg for license
+        '''
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
