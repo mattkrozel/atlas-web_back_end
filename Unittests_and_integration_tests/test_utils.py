3b8ffc2528d044a9a7b924648630b87562a78abc
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-30 15:46:13 -0500

    task 0 submit, first unit test and implemented method to check return

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+'''
+test utils
+'''
+from unittest import TestCase
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(TestCase):
+    '''
+    inheirts from unittest.TestCase
+    arguments TestCase type
+    '''
+    @parameterized.expand(
+        [({'a': 1}, ('a'), 1),
+         ({'a': {'b': 2}}, ('a',), {'b': 2}),
+         ({'a': {'b': 2}}, ('a', 'b'), 2)]
+    )
+    def test_access_nested_map(self, nested_map, path, expected):
+        '''
+        testing method returns correctly
+        '''
+        self.assertEqual(access_nested_map(nested_map, path), expected)
