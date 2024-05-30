8ba5742da3a64aa8f79a333d22099541a9095210
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-30 16:46:09 -0500

    implemetned githuborg test class and method testing get_json

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+'''
+testing client module
+'''
+import unittest
+from unittest.mock import PropertyMock, patch
+from parameterized import parameterized, paramterized_class
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    '''
+    githubOrg ckient tests class
+    '''
+    @parameterized.expand(
+        [('google'), ('abc')]
+    )
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        '''
+        testing github clients returns correctly
+        '''
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
