cbf9909e7afa9b5a982d42a5d559a82ae9b2ef31
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-05-30 17:20:14 -0500

    tasks 5 & 6 attempt

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -24,3 +24,34 @@ class TestGithubOrgClient(unittest.TestCase):
         test_class = GithubOrgClient(input)
         test_class.org()
         mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+
+    def test_public_repos_url(self):
+        '''
+        testing result is expected based on payload
+        '''
+        with patch('client.GithubOrgClient.org'
+                   new_callable=PropertyMock) as mock:
+            payload = {'repos_url': 'World'}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload['repos_url'])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        '''
+        testing listing of repos is expected from chosen payload
+        testing mocked proprty
+        '''
+        json_payload = [{'name': 'Google'}, {'name': 'Twitter'}]
+        mock_json.return_value = json_payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public:
+            mock_public.return_value = 'hello/world'
+            test_class = GithubOrgClient('test')
+            result = test_class.public_repos()
+            check = [i['name'] for i in json_payload]
+            self.assertEqual(result, check)
+            mock_public.assert_called_once()
+            mock_json.assert_called_once()
