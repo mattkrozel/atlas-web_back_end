3214dd6c3eb2ab60a454b63651552cbf741c6acb
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-06-13 10:46:52 -0500

    added script task 11, listing students with less than 80

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- listing students with score less than 80 and no meetings
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting
+SELECT name
+FROM students
+WHERE SCORE < 80
+AND last_meeting IS NULL
+or last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH);
