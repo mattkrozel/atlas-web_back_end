20645d671e48022fe671482414566a54904c66d8
Matt Krozel <mattkrozel@Matts-MacBook-Pro-2.local>
2024-06-13 11:25:19 -0500

    push task 8, listing all

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,3 +3,11 @@
 mongo collection
 pymongo
 '''
+def list_all(mongo_collection: object) -> list:
+    '''
+    listing all docs in collection
+    args 
+    mongo collection object
+    pymongo collection object
+    '''
+    return mongo_collection.find({}) if mongo_collection.find({}) else []
