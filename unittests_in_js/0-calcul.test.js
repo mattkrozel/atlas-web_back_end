febf636d41c1538a8ba2b53aa5a204e2f5677a18
Matt Krozel <mattkrozel@Matts-MBP-2.attlocal.net>
2024-06-27 18:33:48 -0500

    task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -7,4 +7,19 @@ describe('calculateNumber', function () {
       assert.strictEqual(calculateNumber(2, 3), 5);
     });
   });
+  describe('First round', function() {
+    it('should return 5', function () {
+      assert.strictEqual(calculateNumber(3.7, 1), 5);
+    });
+  });
+  describe('Second round', function() {
+    it('should return 5', function () {
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+  });
+  describe('Both round', function() {
+    it('should return 5', function () {
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+  });
 });
