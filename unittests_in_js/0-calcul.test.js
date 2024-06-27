9826bdf3cf5f060d9ed2c1f4f5edcb8dab2bc68b
Matt Krozel <mattkrozel@Matts-MBP-2.attlocal.net>
2024-06-27 18:37:51 -0500

    task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -4,22 +4,27 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function () {
   describe('Two Integers', function() {
     it('should return 5', function () {
-      assert.strictEqual(calculateNumber(2, 3), 5);
+      assert.strictEqual(calculateNumber(1, 3), 4);
     });
   });
   describe('First round', function() {
     it('should return 5', function () {
-      assert.strictEqual(calculateNumber(3.7, 1), 5);
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
   });
   describe('Second round', function() {
     it('should return 5', function () {
-      assert.strictEqual(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
   });
   describe('Both round', function() {
     it('should return 5', function () {
-      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
+  });
+  describe('2 round', function() {
+    it('should return 5', function () {
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
     });
   });
 });
