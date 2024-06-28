2acdc8fc9284c899b358b837dc64e3d5fb313ca0
Matt Krozel <mattkrozel@Matts-MBP-2.attlocal.net>
2024-06-27 19:01:48 -0500

    task 2 tested

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const calculateNumber = require('./2-calcul_chai');
+
+describe('calculateNumber', function () {
+  describe('sum', function() {
+    it('should return 6', function () {
+      chai.expect(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+  describe('subtract', function() {
+    it('should return -4', function () {
+      chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+  describe('divide', function() {
+    it('should return .2', function () {
+      chai.expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+  });
+  describe('divide error', function() {
+    it('should return Error', function () {
+      chai.expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
