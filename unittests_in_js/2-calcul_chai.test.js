68b534256e3572de22763b19e6df130cc2843516
Matt Krozel <mattkrozel@Matts-MBP-2.attlocal.net>
2024-06-27 19:05:25 -0500

    task 2 tested

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -4,22 +4,22 @@ const calculateNumber = require('./2-calcul_chai');
 describe('calculateNumber', function () {
   describe('sum', function() {
     it('should return 6', function () {
-      chai.expect(calculateNumber('SUM', 1.4, 4.5), 6);
+      chai.expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
   });
   describe('subtract', function() {
     it('should return -4', function () {
-      chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      chai.expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
   });
   describe('divide', function() {
     it('should return .2', function () {
-      chai.expect(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      chai.expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
   });
   describe('divide error', function() {
     it('should return Error', function () {
-      chai.expect(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+      chai.expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
   });
 });
