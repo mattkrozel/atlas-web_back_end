ff75efe3c6bf6e0d30e40e99aa410d37403c4eb8
Matt Krozel <mattkrozel@Matts-MBP-2.attlocal.net>
2024-06-27 19:58:42 -0500

    task 6 done

---
+++ unittests_in_js/6-payment_token.js
@@ -0,0 +1,7 @@
+const getPaymentTokenFromAPI = (success) => {
+  if (success === true) {
+    return Promise.resolve({ data: 'Successful response from the API' });
+  }
+  return null;
+};
+module.exports = getPaymentTokenFromAPI;
