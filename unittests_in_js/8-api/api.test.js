d0a82d6dbddd210785bb26ea59529ee717265c56
Matt Krozel <mattkrozel@Matts-MBP-2.attlocal.net>
2024-06-27 20:21:45 -0500

    task 8 fixed

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -7,6 +7,11 @@ describe('Integration Testing', () => {
         url: 'http://localhost:7865',
         method: 'GET',
       };
+      request(options, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
   });
 });
